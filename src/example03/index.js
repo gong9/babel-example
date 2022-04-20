1453fedf459ba63906a4cc8175c2d2ef55ff90c6
gongzhen <gongzhen@bailongma-inc.com>
2022-04-20 17:51:40 +0800

    feat: 代码压缩

---
+++ src/example03/index.js
@@ -0,0 +1,45 @@
+const {
+    transformFromAstSync
+} = require('@babel/core');
+const parser = require('@babel/parser');
+const prettier = require('prettier')
+const compressPlugin = require('./plugin/compress');
+
+let sourceCode = `
+    function func() {
+        const num1 = 1
+        const num2 = 2
+        const num3 = add(1, 2)
+        const num4 = add(3, 4)
+        console.log(num2)
+        return num2
+        console.log(num1)
+        function add (aaa, bbb) {
+            return aaa + bbb
+        }
+    }
+    func()
+`;
+
+sourceCode = prettier.format(sourceCode, {
+    semi: true,
+    parser: "babel"
+});
+
+const ast = parser.parse(sourceCode, {
+    sourceType: 'unambiguous',
+    comments: true
+});
+
+const {
+    code
+} = transformFromAstSync(ast, sourceCode, {
+    plugins: [
+        [compressPlugin]
+    ],
+    generatorOpts: {
+        comments: false,
+        compact: true
+    }
+});
+console.log(code);
